0ca21ce64f238f88a89259f53d3423cbeb57a344
aplavsa <andrijaplavsa@gmail.com>
2016-10-03 11:48:12 +0200

    Update Program.cs

--- pzw/pzw/Program.cs
+++ pzw/pzw/Program.cs
@@ -10,7 +10,7 @@ namespace pzw
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Di si?");
         }
     }
 }
